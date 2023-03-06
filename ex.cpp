4c89ca993c5da7a1a7f451a21970a928117931ae
Dianitsya <dianitasyanandamasta@gmail.com>
2023-03-06 15:45:34 +0700

    first commit

---
+++ ex.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    double p, l, Luas;
+    cout << "masukkkan panjang: ";
+    cin >> p;
+    cout << "masukkkan lebar: ";
+    cin >> l;
+    Luas = p * l;
+    cout << "luasnya adalah " << Luas;
+}
